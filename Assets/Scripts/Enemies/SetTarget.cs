40577e6982c20879caf40b536e651a11d9d334f4
CLEM\konst <konstantytadeusz@gmail.com>
2024-12-09 22:42:17 +0100

    ai resolved

--- Assets/Scripts/Enemies/SetTarget.cs
+++ Assets/Scripts/Enemies/SetTarget.cs
@@ -1,19 +1,18 @@
 using UnityEngine;
-using UnityEngine.AI;
+using DG.Tweening;
 
 public class SetTarget : MonoBehaviour
 {
-    [SerializeField] NavMeshAgent agent;
+    [SerializeField] private float speed;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.DOMove(PlayerSingleton._player.transform.position, speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(PlayerSingleton._player.transform.position);    
     }
 }
