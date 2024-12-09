ae9d4d8c6fde52b1286553ecf038dccc59f6fcb9
CLEM\konst <konstantytadeusz@gmail.com>
2024-12-09 19:28:07 +0100

    Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    Actions actions;
+    [SerializeField]float speed;
+    InputAction movement;
+    Transform player;
+
+    void Awake()
+    {
+        actions = new Actions();
+    }
+
+    void OnEnable()
+    {
+        actions.Enable();
+        movement = actions.Player.Move;
+        player = transform;
+    }
+
+    void OnDisable()
+    {
+        actions.Disable();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        player.transform.Translate(new Vector3(movement.ReadValue<Vector2>().x * speed, movement.ReadValue<Vector2>().y * speed, 0));
+    }
+}
